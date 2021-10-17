79b50b0f1bfcf37151fe01963b85f71d535dec97
Dave Cushing <dave.cushing@cambriancollege.ca>
2021-10-17 19:27:35 -0400

    midterm

--- Linux/midterm/midterm-setup1.sh
+++ Linux/midterm/midterm-setup1.sh
@@ -27,10 +27,16 @@ blank_line
 echo -e "Setting up..."
 #
 # Prep Apache back to defaults
+# Copy files for midterm
 #
 echo -e "..Cleaning Apache Sites..."
 systemctl apache2 stop
+cp /multiplatform/Linux/ssl-server-nopass.key /etc/ssl/ 2>/dev/null
+chmod 644 /etc/ssl/Linux/ssl-server-nopass.key
+cp /multiplatform/Linux/www.midterm.org.pem /etc/ssl/ 2>/dev/null
+chmod 644 /etc/ssl/www.midterm.org.pem
 mkdir /etc/apache2/sites-enabled/midterm_backup 2>/dev/null
+mkdir /var/www/midterm.org 2>/dev/null
 mv /etc/apache2/sites-enabled/*.conf /etc/apache2/sites-enabled/midterm_backup/ 2>/dev/null
 ln -s /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-enabled/000-default.conf
 blank_line
@@ -39,9 +45,11 @@ systemctl start apache2
 apache2ctl -t -D DUMP_VHOSTS
 #
 # Prep Bind back to defaults
+# Copy files for midterm
 #
 echo -e "....Cleaning Bind Settings..."
 systemctl stop bind9
+iptables -I INPUT -s localhost -d 127.0.0.53 -j ACCEPT
 mv /etc/bind/named.conf.options /etc/bind/named.conf.options.midterm
 mv /etc/bind/named.conf.local /etc/bind/named.conf.local.midterm
 cp /multiplatform/Linux/midterm/named.conf.options.midterm /etc/bind/named.conf.options
@@ -57,4 +65,10 @@ chmod 644 /var/cache/bind/example.com
 systemctl start bind9
 named-checkconf -z /etc/bind/named.conf
 blank_line
+dig @localhost +noall +answer ns1.midterm.org
+dig @localhost +noall +answer ns2.midterm.org
+dig @localhost +noall +answer ubuntu1804.midterm.org
+dig @localhost +noall +answer windows2016.midterm.org
+dig @localhost +noall +answer host1.midterm.org
+dig @localhost +noall +answer host2.midterm.org
 echo -e "[[[[ END CUT AND PASTE ]]]]"
