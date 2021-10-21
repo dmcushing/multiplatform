812783ff1bf8edbb019d0e21eadf49f499ab88cb
Dave Cushing <dave.cushing@cambriancollege.ca>
2021-10-21 14:43:55 -0400

    midterm

--- Linux/midterm/midterm-setup.sh
+++ Linux/midterm/midterm-setup.sh
@@ -31,6 +31,8 @@ echo -e "---Apache Midterm Site---"
 cp -f /multiplatform/Linux/midterm/midterm.org.conf /etc/apache2/sites-enabled/
 cp -f /multiplatform/Linux/midterm/apacheindex.html /var/www/midterm.org/index.html
 cp -f /multiplatform/Linux/midterm/apacherror.html /var/www/midterm.org/
+cp -f /multiplatform/Linux/midterm/midterm.org /var/cache/bind/
+chmod 644 /var/cache/bind/midterm.org
 chmod 666 /etc/apache2/sites-enabled/midterm.org.conf
 chmod 666 /var/www/midterm.org/index.html
 chmod 666 /var/www/midterm.org/apacherror.html
