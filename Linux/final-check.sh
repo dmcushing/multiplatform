0745f8224419a70bbfaeb3b8820a8054434f3b1a
Dave Cushing <dave.cushing@cambriancollege.ca>
2021-12-11 11:35:06 -0500

    final

--- Linux/final-check.sh
+++ Linux/final-check.sh
@@ -2,32 +2,33 @@
 # Final Exam
 
 source /multiplatform/Linux/functions.sh
-
+gzip -d /multiplatform/Linux/sendgrid.gz.sh 2>&1>/dev/null
+source /multiplatform/Linux/sendgrid.sh
 # Gather Student Work
 
 clear
 is_super_user
 student_info 2420_Final_Exam
+echo -n " Enter your Ubuntu internal IP: "
+read ubuntuip
 echo -n "Enter your Windows internal IP: "
 read winip
-echo -n "Enter your Ubuntu internal IP: "
-read ubuntuip
 
 # bind
 
 echo -e "bind checks: Ubuntu" | tee -a $outfile
-echo -e "www.cet2420-final.org: `dig @localhost +noall +answer www.cet2420-final.org`" | tee -a $outfile
-dig @localhost +noall +answer ubuntu.cet2420-final.org | tee -a $outfile
-dig @localhost +noall +answer linux.cet2420-final.org | tee -a $outfile
-dig @localhost +noall +answer mail.cet2420-final.org | tee -a $outfile
-dig @localhost +noall +answer cet2420-final.org MX | tee -a $outfile
-dig @localhost +noall +answer windows.cet2420-final.org | tee -a $outfile
-dig @localhost +noall +answer s2016.cet2420-final.org | tee -a $outfile
+echo -e "           www.cet2420-final.org: `dig @localhost +noall +answer www.cet2420-final.org`" | tee -a $outfile
+echo -e "        ubuntu.cet2420-final.org: `dig @localhost +noall +answer ubuntu.cet2420-final.org`" | tee -a $outfile
+echo -e "         linux.cet2420-final.org: `dig @localhost +noall +answer linux.cet2420-final.org`" | tee -a $outfile
+echo -e "          mail.cet2420-final.org: `dig @localhost +noall +answer mail.cet2420-final.org`" | tee -a $outfile
+echo -e "mail.cet2420-final.org MX Record: `dig @localhost +noall +answer cet2420-final.org MX`" | tee -a $outfile
+echo -e "       windows.cet2420-final.org: `dig @localhost +noall +answer windows.cet2420-final.org`" | tee -a $outfile
+echo -e "         s2016.cet2420-final.org: `dig @localhost +noall +answer s2016.cet2420-final.org`" | tee -a $outfile
 blank_line
 echo -e "bind checks: Windows" | tee -a $outfile
 echo -e "Primary:" | tee -a $outfile
-dig @$winip +noall +answer final.cet2420-win.org | tee -a $outfile
-dig @$winip +noall +answer tux.cet2420-win.org | tee -a $outfile
+echo -e "           final.cet2420-win.org: `dig @$winip +noall +answer final.cet2420-win.org`" | tee -a $outfile
+echo -e "             tux.cet2420-win.org: `dig @$winip +noall +answer tux.cet2420-win.org`" | tee -a $outfile
 echo -e "Secondary:" | tee -a $outfile
 dig @$winip +noall +answer www.cet2420-final.org | tee -a $outfile
 dig @$winip +noall +answer ubuntu.cet2420-final.org | tee -a $outfile
@@ -58,21 +59,21 @@ echo -e "nfs check"
 exportfs -v | tee -a $outfile
 blank_line
 
-echo -e "CONFIG FILES" | tee -a $outfile
-echo -e "/etc/bind/named.conf.local" | tee -a $outfile
-cat /etc/bind/named.conf.local | tee -a $outfile
-blank_line
-echo -e "/var/cache/bind/cet2420-final.org" | tee -a $outfile
-cat /var/cache/bind/cet2420-final.org | tee -a $outfile
-blank_line
-echo -e "/etc/apache2/sites-enabled/cet2420-final.org.conf" | tee -a $outfile
-cat /etc/apache2/sites-enabled/cet2420-final.org.conf | tee -a $outfile
-blank_line
-echo -e "/etc/dhcp/dhcpd.conf" | tee -a $outfile
-cat /etc/dhcp/dhcpd.conf | tee -a $outfile
-blank_line
-echo -e "/etc/exports"
-cat /etc/exports | tee -a $outfile
-blank_line
+echo -e "CONFIG FILES" > $outfile
+echo -e "/etc/bind/named.conf.local" > $outfile
+cat /etc/bind/named.conf.local > $outfile
+echo -e " " > $outfile
+echo -e "/var/cache/bind/cet2420-final.org" > $outfile
+cat /var/cache/bind/cet2420-final.org > $outfile
+echo -e " " > $outfile
+echo -e "/etc/apache2/sites-enabled/cet2420-final.org.conf" > $outfile
+cat /etc/apache2/sites-enabled/cet2420-final.org.conf > $outfile
+echo -e " " > $outfile
+echo -e "/etc/dhcp/dhcpd.conf" > $outfile
+cat /etc/dhcp/dhcpd.conf > $outfile
+echo -e " " > $outfile
+echo -e "/etc/exports" > $outfile
+cat /etc/exports > $outfile
+echo -e " " > $outfile
 
 mail_out 2420 Final Exam
